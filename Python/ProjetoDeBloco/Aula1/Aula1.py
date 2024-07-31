75b078f32ffa433fdeb9721040ef3e4032a5ff02
Carlos Vinícius <p.carlosvini@gmail.com>
2024-07-30 21:02:08 -0300

    add len variable

--- Python/ProjetoDeBloco/Aula1/Aula1.py
+++ Python/ProjetoDeBloco/Aula1/Aula1.py
@@ -1,3 +1,7 @@
 texto = 'Olá, mundo!' #Atribuição de variável do tipo string
 
 print(texto) #Impressão da variável texto
+
+quantidade = len(texto) #Atribuição de variável do tipo inteiro com a quantidade de caracteres da variável texto
+
+print(quantidade, 'Caracteres') #Impressão da variável quantidade
