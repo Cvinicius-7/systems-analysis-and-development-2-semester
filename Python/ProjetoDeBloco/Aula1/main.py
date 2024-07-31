c58cb2e29a96e251d107afba0b1331059ed2d031
Carlos Vinícius <p.carlosvini@gmail.com>
2024-07-30 21:10:35 -0300

    Add input example

--- Python/ProjetoDeBloco/Aula1/main.py
+++ Python/ProjetoDeBloco/Aula1/main.py
@@ -1,7 +1,5 @@
 texto = 'Olá, mundo!' #Atribuição de variável do tipo string
-
 print(texto) #Impressão da variável texto
 
 quantidade = len(texto) #Atribuição de variável do tipo inteiro com a quantidade de caracteres da variável texto
-
 print(quantidade, 'Caracteres') #Impressão da variável quantidade
---
+++ Python/ProjetoDeBloco/Aula1/num.py
@@ -0,0 +1,2 @@
+num1 = int(input("Digite um número inteiro: "))
+print('Número:', num1)
