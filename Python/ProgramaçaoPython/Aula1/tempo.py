6fefc480c5de55c4c37d8edd5c21f208d70da518
Carlos Vinícius <p.carlosvini@gmail.com>
2024-07-31 21:59:19 -0300

    better example

--- Python/ProgramaçaoPython/Aula1/tempo.py
+++ Python/ProgramaçaoPython/Aula1/tempo.py
@@ -1,15 +1,22 @@
 
 min = 386
+
 hrs = min/60
+hrs =round(hrs, 2)
+
 seg = min*60
+seg = round(seg, 2)
 
 dia =(min/60)/24
+dia = round(dia, 2)
 
-print(min)
-print(" minutos equivalem a")
-print(dia)
-print(" dias")
-print(hrs)
-print(" horas")
-print(seg)
-print(" segundos")
+#print(min)
+#print(" minutos equivalem a")
+#print(dia)
+#print(" dias")
+#print(hrs)
+#print(" horas")
+#print(seg)
+#print(" segundos")
+
+print(min, "minutos equivalem a", dia, "dias", hrs, "horas e", seg, "segundos")
