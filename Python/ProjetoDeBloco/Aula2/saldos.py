e1f43432eec8bda7a69845943c5de68cbc82c7b6
Carlos Vinícius <p.carlosvini@gmail.com>
2024-08-07 22:21:34 -0300

    Add example

---
+++ Python/ProjetoDeBloco/Aula2/saldos.py
@@ -0,0 +1,17 @@
+saldo = 50
+saldo = saldo + 100
+print(saldo)
+print()
+jan = 10
+fev = 20.5
+mar = 20.356
+abr = 10.24
+mai = 10.8
+total = jan + fev + mar + abr + mai
+media = total / 5
+print('Total:',total)
+print('Média:',media)
+print()
+print('total Arredondado:',round(total,2))
+print('Média Arredondada:',round(media,2))
+    
