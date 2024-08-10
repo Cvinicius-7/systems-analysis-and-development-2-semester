66c29b695c2d34e2179b850cc9e29548a3c2648e
Carlos Vinícius <p.carlosvini@gmail.com>
2024-08-10 13:03:12 -0300

    Add Exercise 4 Resolution

--- Python/ProgramaçaoPython/Tp1/Exercicio4.py
+++ Python/ProgramaçaoPython/Tp1/Exercicio4.py
@@ -1,1 +1,22 @@
 #Desenvolva um programa que peça ao usuário para escolher uma operação (adição, subtração, multiplicação, divisão) e dois números, e então execute a operação escolhida com os números.
+
+primeiro_numero = input("Digite o primeiro número: ")
+segundo_numero = input("Digite o segundo número: ")
+operacao = input("Escolha sua operação: \n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n")
+
+match operacao:
+    case "1":
+        resultado = round(float(primeiro_numero) + float(segundo_numero),2)
+        print("Soma:", resultado)
+    case "2":
+        resultado = float(primeiro_numero) - float(segundo_numero)
+        print("Subtração:", resultado)
+    case "3":
+        resultado = float(primeiro_numero) * float(segundo_numero)
+        print("Multiplicação:", resultado)
+    case "4":
+        resultado = round(float(primeiro_numero) / float(segundo_numero),2)
+        print("Divisão:", resultado)
+    case _:
+        print("Digite uma operação válida.")
+   
