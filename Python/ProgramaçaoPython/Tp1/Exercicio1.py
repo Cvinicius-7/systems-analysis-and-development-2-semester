a0fd0127c3a6c17edd211eafdb58906d8c780b99
Carlos Vinícius <p.carlosvini@gmail.com>
2024-08-10 12:30:15 -0300

    Add resolution Exercise 1

--- Python/ProgramaçaoPython/Tp1/Exercicio1.py
+++ Python/ProgramaçaoPython/Tp1/Exercicio1.py
@@ -1,5 +1,18 @@
 #1 Crie um programa que peça ao usuário para inserir dois números e, em seguida, calcule e exiba a soma, subtração, multiplicação, divisão e divisão inteira desses números.
 
+primeiro_numero = input("Digite o primeiro número: ")
+segundo_numero = input("Digite o segundo número: ")
+soma = round(float(primeiro_numero) + float(segundo_numero),2)
+subtracao = float(primeiro_numero) - float(segundo_numero)
+multiplicacao = float(primeiro_numero) * float(segundo_numero)
+divisao = round(float(primeiro_numero) / float(segundo_numero),2)
+divisao_inteira = float(primeiro_numero) // float(segundo_numero)
+
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
+print("Divisão inteira:", divisao_inteira)
 
 
 
