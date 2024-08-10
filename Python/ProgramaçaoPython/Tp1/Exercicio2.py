4f73c24a0827605613d73393e94f38960b69433e
Carlos Vinícius <p.carlosvini@gmail.com>
2024-08-10 13:10:14 -0300

    Exercise 6 Resolution

--- Python/ProgramaçaoPython/Tp1/Exercicio2.py
+++ Python/ProgramaçaoPython/Tp1/Exercicio2.py
@@ -1,1 +1,2 @@
 #Faça um programa que converta um número fornecido de minutos em horas e minutos, e depois faça o inverso, convertendo horas e minutos de volta para minutos totais.
+
--- Python/ProgramaçaoPython/Tp1/Exercicio6.py
+++ Python/ProgramaçaoPython/Tp1/Exercicio6.py
@@ -1,1 +1,14 @@
 #Escreva um programa onde o usuário deve adivinhar um número secreto. O programa deve dizer se o palpite está correto, muito alto ou muito baixo.
+
+num_secreto = 42
+
+palpite = int(input("Digite seu palpite: "))
+
+while palpite != num_secreto:
+    if palpite > num_secreto:
+        print("Muito alto.")
+    else:
+        print("Muito baixo.")
+    palpite = int(input("Digite seu palpite: "))
+    
+print("Parabéns! Você acertou o número secreto.")
