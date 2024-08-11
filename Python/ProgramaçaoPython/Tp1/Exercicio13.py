25b82b8eff8cf9b3309c01dcbe08e9928e6dc27c
Carlos Vinícius <p.carlosvini@gmail.com>
2024-08-11 20:24:44 -0300

    add exercise 13 resolution

--- Python/ProgramaçaoPython/Tp1/Exercicio13.py
+++ Python/ProgramaçaoPython/Tp1/Exercicio13.py
@@ -1,1 +1,8 @@
 #Desenvolva um programa que verifique se uma palavra ou frase inserida pelo usuário é um palíndromo (lê-se igual de trás para frente).
+
+palavra = input("Digite uma palavra ou frase: ").lower()
+
+if palavra == palavra[::-1]:
+    print("Esta palavra ou frase é um palíndromo.")
+else:
+    print("Esta palavra ou frase não é um palíndromo.")
