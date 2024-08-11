c17104d17acd186b982b3166e1799c9442dde941
Carlos Vinícius <p.carlosvini@gmail.com>
2024-08-11 20:09:21 -0300

    Add Exercise 3 Resolution

--- Python/ProgramaçaoPython/Tp1/Exercicio3.py
+++ Python/ProgramaçaoPython/Tp1/Exercicio3.py
@@ -1,1 +1,6 @@
 #Escreva um programa que receba dois nomes de usuário e os combine de maneira criativa para formar um novo nome.
+
+primeiro_nome = input("Digite o primeiro nome: ")
+segundo_nome = input("Digite o segundo nome: ")
+
+print("Nome criativo:", primeiro_nome[:3] + segundo_nome[-3:])
