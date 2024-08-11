ed56120575e38faf18a1d6ee774bbac6af310d6a
Carlos Vinícius <p.carlosvini@gmail.com>
2024-08-11 20:19:01 -0300

    Add Exercise 11

--- Python/ProgramaçaoPython/Tp1/Exercicio11.py
+++ Python/ProgramaçaoPython/Tp1/Exercicio11.py
@@ -1,1 +1,9 @@
 #Faça um programa que simule o lançamento de um dado. O usuário deve escolher quantos dados jogar e o programa deve exibir os resultados.
+
+import random
+
+dados = int(input("Quantos dados você quer jogar? "))
+
+for i in range(dados):
+    print("Dado", i+1, ":", random.randint(1, 6))
+    
