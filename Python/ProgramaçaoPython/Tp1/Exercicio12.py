137ce829042ba0535e0ad90f8f1632051f86c818
Carlos Vinícius <p.carlosvini@gmail.com>
2024-08-11 20:21:18 -0300

    Add Exercise 12 resolutinon

--- Python/ProgramaçaoPython/Tp1/Exercicio12.py
+++ Python/ProgramaçaoPython/Tp1/Exercicio12.py
@@ -1,1 +1,9 @@
 #Crie um programa que classifique as palavras inseridas pelo usuário como curtas (menos de 5 letras) ou longas (5 letras ou mais).
+
+palavras = input("Digite algumas palavras separadas por espaços: ").split()
+
+for palavra in palavras:
+    if len(palavra) < 5:
+        print(palavra, "é uma palavra curta.")
+    else:
+        print(palavra, "é uma palavra longa.")
