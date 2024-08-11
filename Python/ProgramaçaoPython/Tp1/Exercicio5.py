a43f3e8d81202c7ac744157ba78414c794b36fdb
Carlos Vinícius <p.carlosvini@gmail.com>
2024-08-11 20:15:09 -0300

    Add Exercise 5 Resolution

--- Python/ProgramaçaoPython/Tp1/Exercicio5.py
+++ Python/ProgramaçaoPython/Tp1/Exercicio5.py
@@ -1,1 +1,6 @@
 #Crie um programa que peça ao usuário seu nome e sobrenome usando input e, em seguida, combine-os para formar uma saudação personalizada.
+
+primeiro_nome = input("Digite seu primeiro nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+
+print("Um ótimo dia,", primeiro_nome, sobrenome + "!")
