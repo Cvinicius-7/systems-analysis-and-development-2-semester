f53a3dc77fb0581b9ba1c151ed833d0c9c7d1c52
Carlos Vinícius <p.carlosvini@gmail.com>
2024-08-11 21:08:45 -0300

    add exercise 9

--- Python/ProgramaçaoPython/Tp1/Exercicio9.py
+++ Python/ProgramaçaoPython/Tp1/Exercicio9.py
@@ -1,1 +1,14 @@
 #Desenvolva um programa que aplique descontos progressivos com base no valor da compra: desconto de 10% para compras acima de R$100, 15% para compras acima de R$200, seguindo a projeção até R$500, para valores maiores do que esse, o desconto é fixado em 25%.
+
+desconto = 0
+valor_compra = float(input("Digite o valor da compra: "))
+
+if valor_compra > 500:
+    desconto = 0.25 * valor_compra
+elif valor_compra > 200:
+    desconto = 0.15 * valor_compra
+elif valor_compra > 100:
+    desconto = 0.10 * valor_compra
+    
+    print("Desconto de R$", desconto)
+    print("Valor final: R$", valor_compra - desconto)
