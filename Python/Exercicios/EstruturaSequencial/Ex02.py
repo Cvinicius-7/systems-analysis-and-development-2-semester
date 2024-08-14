04f124b216830c1af8d6fb2cff0ed15b1395df92
Carlos Vinícius <p.carlosvini@gmail.com>
2024-08-13 22:54:17 -0300

    Ex 02 Resolution

---
+++ Python/Exercicios/EstruturaSequencial/Ex02.py
@@ -0,0 +1,7 @@
+#Faça um Programa que converta metros para centímetros.
+
+metros = int(input("Digite a quantidade de metros: "))
+
+centimetros = metros * 100
+
+print("A quantidade de metros em centímetros é:", centimetros,"Centimetros")
