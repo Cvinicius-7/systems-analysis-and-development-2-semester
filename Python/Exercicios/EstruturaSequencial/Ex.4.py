2e191595aa778402910d8baa5bcd3206079324f5
Carlos Vinícius <p.carlosvini@gmail.com>
2024-08-13 23:14:48 -0300

    add question

--- Python/Exercicios/EstruturaSequencial/Ex.4.py
+++ Python/Exercicios/EstruturaSequencial/Ex.4.py
@@ -1,4 +1,4 @@
-
+#Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
 tamanho_arquivo = float(input("Digite o tamanho do arquivo em MB: "))
 velocidade_internet = float(input("Digite a velocidade da internet em Mbps: "))
 
