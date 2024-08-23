fe5c587d02889e8c9b37746ae455e1ffe4dc9d32
Carlos Vinícius <p.carlosvini@gmail.com>
2024-08-22 22:05:58 -0300

    Ex 5 Resoluton

---
+++ Python/ProgramaçaoPython/Tp2/NivelMinhoquinha/Ex5.1.py
@@ -0,0 +1,6 @@
+#Crie uma função chamada saudacao imprima uma saudação personalizada, como "Olá, [nome do usuário]!", recebendo o nome como argumento da função.
+
+def saudacao(nome):
+    print("Olá,", nome +"!")
+    
+saudacao(input("Digite seu nome: "))
