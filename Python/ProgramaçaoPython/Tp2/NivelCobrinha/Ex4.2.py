0eba873bbeb1acba7329fc940935b56c51b16242
Carlos Vinícius <p.carlosvini@gmail.com>
2024-08-22 22:50:40 -0300

    Ex 4 Resolution

---
+++ Python/ProgramaçaoPython/Tp2/NivelCobrinha/Ex4.2.py
@@ -0,0 +1,16 @@
+"""""
+Para cada uma das funções definidas nas questões anteriores, adicione um DocString que explique o propósito da função, seus argumentos e o valor de retorno, se houver, seguindo a folha de estilo exemplificada abaixo. [5.6]
+
+Exemplo de folha de estilo do DocString:
+def soma(numero_a, numero_b):
+|
+| Realiza a soma entre dois números
+|
+| Args:
+| numero_a (int | float): Primeiro número a ser considerado.
+| numero_b (int | float): Segundo número a ser considerado.
+|
+| Return:
+| (int | float): Resultado da soma.
+| """
+# Código de implementação da função.
