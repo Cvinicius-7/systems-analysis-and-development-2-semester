f876b4348d51b90648e2b91a17947a785944acc5
Carlos Vinícius <p.carlosvini@gmail.com>
2024-09-09 20:37:09 -0300

    create first button row scheme

--- Python/Exercicios/Calculadora/main.py
+++ Python/Exercicios/Calculadora/main.py
@@ -12,7 +12,7 @@ blue = "#102ac2"; #azul
 #Criando a janela
 window = Tk();
 window.title("Calculadora");
-window.geometry("300x400");
+window.geometry("287x400");
 window.configure(bg=gray);
 
 #Criando o frame
@@ -24,7 +24,21 @@ window_frame2 = Frame(window, width=300, height=330,);
 window_frame2.grid(row=1, column=0,);
 
 #botões
-but1 = Button(window_frame2, text="C", width=10, height=3, bg=white);
+but1 = Button(window_frame2, text="C", width=14, height=3, bg=white, font=("ivy 13 bold"), relief=RAISED, overrelief=RIDGE);
 but1.place(x=0, y=0);
+but2 = Button(window_frame2, text="%", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but2.place(x=146, y=0);
+but3 = Button(window_frame2, text="/", width=7, height=3, bg=orange, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but3.place(x=219, y=0);
+
+but4 = Button(window_frame2, text="7", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but4.place(x=0, y=70);
+but5 = Button(window_frame2, text="8", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but5.place(x=73, y=70);
+but6 = Button(window_frame2, text="9", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but6.place(x=146, y=70);
+but7 = Button(window_frame2, text="*", width=7, height=3, bg=orange, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but7.place(x=219, y=70);
+
 
 window.mainloop()
