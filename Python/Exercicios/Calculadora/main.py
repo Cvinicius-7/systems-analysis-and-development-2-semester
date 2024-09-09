f9b2f95115f58088b4ce65b0d1ded8c37a9b3469
Carlos Vinícius <p.carlosvini@gmail.com>
2024-09-09 19:50:18 -0300

    create window frame

--- Python/Exercicios/Calculadora/main.py
+++ Python/Exercicios/Calculadora/main.py
@@ -7,5 +7,8 @@ window = Tk()
 window.title("Calculadora")
 window.geometry("300x400")
 
+#Criando o frame
+window_frame = Frame(window, width=300, height=40)
+window.frame.grid(row=0, column=0)
 
 window.mainloop()
