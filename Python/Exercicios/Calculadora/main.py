e86ac716b3a1b87bd47c0dfb77a3b4551de9e769
Carlos Vinícius <p.carlosvini@gmail.com>
2024-09-09 20:46:00 -0300

    add last buttons and organize window dimension

--- Python/Exercicios/Calculadora/main.py
+++ Python/Exercicios/Calculadora/main.py
@@ -12,7 +12,7 @@ blue = "#102ac2"; #azul
 #Criando a janela
 window = Tk();
 window.title("Calculadora");
-window.geometry("287x400");
+window.geometry("287x420");
 window.configure(bg=gray);
 
 #Criando o frame
@@ -20,7 +20,7 @@ window_frame = Frame(window, width=300, height=70, bg=gray);
 window_frame.grid(row=0, column=0,);
 
 #Segundo frame
-window_frame2 = Frame(window, width=300, height=330,);
+window_frame2 = Frame(window, width=300, height=420);
 window_frame2.grid(row=1, column=0,);
 
 #botões
@@ -40,5 +40,29 @@ but6.place(x=146, y=70);
 but7 = Button(window_frame2, text="*", width=7, height=3, bg=orange, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
 but7.place(x=219, y=70);
 
+but8 = Button(window_frame2, text="4", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but8.place(x=0, y=140);
+but9 = Button(window_frame2, text="5", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but9.place(x=73, y=140);
+but10 = Button(window_frame2, text="6", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but10.place(x=146, y=140);
+but11 = Button(window_frame2, text="-", width=7, height=3, bg=orange, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but11.place(x=219, y=140);
+
+but12 = Button(window_frame2, text="1", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but12.place(x=0, y=210);
+but13 = Button(window_frame2, text="2", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but13.place(x=73, y=210);
+but14 = Button(window_frame2, text="3", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but14.place(x=146, y=210);
+but15 = Button(window_frame2, text="+", width=7, height=3, bg=orange, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but15.place(x=219, y=210);
+
+but16 = Button(window_frame2, text="0", width=14, height=3, bg=white, font=("ivy 13 bold"), relief=RAISED, overrelief=RIDGE);
+but16.place(x=0, y=280);
+but17 = Button(window_frame2, text=".", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but17.place(x=146, y=280);
+but18 = Button(window_frame2, text="=", width=7, height=3, bg=orange, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but18.place(x=219, y=280);
 
 window.mainloop()
