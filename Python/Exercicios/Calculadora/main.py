7ddb54cea9902932b72dc1fcf7044fd138686bd8
Carlos Vinícius <p.carlosvini@gmail.com>
2024-09-09 19:57:34 -0300

    define colors and change background

--- Python/Exercicios/Calculadora/main.py
+++ Python/Exercicios/Calculadora/main.py
@@ -4,14 +4,19 @@ from tkinter import ttk
 
 #Definindo cores
 gray = "#4b524d" #cinza
+white = "#ffffff" #branco
+black = "#000000" #preto
+orange = "#ff8c00" #laranja
+blue = "#102ac2" #azul
 
 #Criando a janela
 window = Tk()
 window.title("Calculadora")
 window.geometry("300x400")
+window.configure(bg=gray)
 
 #Criando o frame
-window_frame = Frame(window, width=300, height=70, bg= gray)
+window_frame = Frame(window, width=300, height=70,)
 window_frame.grid(row=0, column=0)
 
 window.mainloop()
