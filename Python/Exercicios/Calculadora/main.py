6c762ccb5187f77a0159b743f4624ab136150883
Carlos Vinícius <p.carlosvini@gmail.com>
2024-09-09 21:22:11 -0300

    create first logic function

--- Python/Exercicios/Calculadora/main.py
+++ Python/Exercicios/Calculadora/main.py
@@ -25,50 +25,69 @@ window_frame2 = Frame(window, width=300, height=420);
 window_frame2.grid(row=1, column=0,);
 
 #Criando label
-label = Label(window_frame, text="123456789", bg=gray, font=("ivy 20 bold"), width=16, height=2, padx=7, anchor=E, relief= FLAT, fg=white, justify=RIGHT);
+text_val = StringVar();
+label = Label(window_frame, textvariable = text_val, bg=gray, font=("ivy 20 bold"), width=16, height=2, padx=7, anchor=E, relief= FLAT, fg=white, justify=RIGHT);
 label.place(x=0, y=0);
  
 
 #botões
 but1 = Button(window_frame2, text="C", width=14, height=3, bg=white, font=("ivy 13 bold"), relief=RAISED, overrelief=RIDGE);
 but1.place(x=0, y=0);
-but2 = Button(window_frame2, text="%", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but2 = Button(window_frame2, command= lambda: input("%"), text="%", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
 but2.place(x=146, y=0);
-but3 = Button(window_frame2, text="/", width=7, height=3, bg=orange, font=("ivy 13 "),fg = white, relief=RAISED, overrelief=RIDGE);
+but3 = Button(window_frame2, command= lambda: input("/"),text="/", width=7, height=3, bg=orange, font=("ivy 13 "),fg = white, relief=RAISED, overrelief=RIDGE);
 but3.place(x=219, y=0);
 
-but4 = Button(window_frame2, text="7", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but4 = Button(window_frame2, command= lambda: input("7"),text="7", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
 but4.place(x=0, y=70);
-but5 = Button(window_frame2, text="8", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but5 = Button(window_frame2, command= lambda: input("8"),text="8", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
 but5.place(x=73, y=70);
-but6 = Button(window_frame2, text="9", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but6 = Button(window_frame2, command= lambda: input("9"),text="9", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
 but6.place(x=146, y=70);
-but7 = Button(window_frame2, text="*", width=7, height=3, bg=orange, font=("ivy 13 "),fg = white, relief=RAISED, overrelief=RIDGE);
+but7 = Button(window_frame2, command= lambda: input("*"),text="*", width=7, height=3, bg=orange, font=("ivy 13 "),fg = white, relief=RAISED, overrelief=RIDGE);
 but7.place(x=219, y=70);
 
-but8 = Button(window_frame2, text="4", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but8 = Button(window_frame2, command= lambda: input("4"),text="4", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
 but8.place(x=0, y=140);
-but9 = Button(window_frame2, text="5", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but9 = Button(window_frame2, command= lambda: input("5"),text="5", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
 but9.place(x=73, y=140);
-but10 = Button(window_frame2, text="6", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but10 = Button(window_frame2, command= lambda: input("6"),text="6", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
 but10.place(x=146, y=140);
-but11 = Button(window_frame2, text="-", width=7, height=3, bg=orange, font=("ivy 13 "),fg = white, relief=RAISED, overrelief=RIDGE);
+but11 = Button(window_frame2, command= lambda: input("-"),text="-", width=7, height=3, bg=orange, font=("ivy 13 "),fg = white, relief=RAISED, overrelief=RIDGE);
 but11.place(x=219, y=140);
 
-but12 = Button(window_frame2, text="1", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but12 = Button(window_frame2, command= lambda: input("1"),text="1", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
 but12.place(x=0, y=210);
-but13 = Button(window_frame2, text="2", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but13 = Button(window_frame2, command= lambda: input("2"),text="2", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
 but13.place(x=73, y=210);
-but14 = Button(window_frame2, text="3", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but14 = Button(window_frame2, command= lambda: input("3"),text="3", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
 but14.place(x=146, y=210);
-but15 = Button(window_frame2, text="+", width=7, height=3, bg=orange, font=("ivy 13 "),fg = white, relief=RAISED, overrelief=RIDGE);
+but15 = Button(window_frame2, command= lambda: input("+"),text="+", width=7, height=3, bg=orange, font=("ivy 13 "),fg = white, relief=RAISED, overrelief=RIDGE);
 but15.place(x=219, y=210);
 
-but16 = Button(window_frame2, text="0", width=16, height=3, bg=white, font=("ivy 13"), relief=RAISED, overrelief=RIDGE);
+but16 = Button(window_frame2, command= lambda: input("0"),text="0", width=16, height=3, bg=white, font=("ivy 13"), relief=RAISED, overrelief=RIDGE);
 but16.place(x=0, y=280);
-but17 = Button(window_frame2, text=".", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
+but17 = Button(window_frame2, command= lambda: input("."),text=".", width=7, height=3, bg=white, font=("ivy 13 "), relief=RAISED, overrelief=RIDGE);
 but17.place(x=146, y=280);
-but18 = Button(window_frame2, text="=", width=7, height=3, bg=orange, font=("ivy 13 "),fg = white, relief=RAISED, overrelief=RIDGE);
+but18 = Button(window_frame2, command= lambda: input("="),text="=", width=7, height=3, bg=orange, font=("ivy 13 "),fg = white, relief=RAISED, overrelief=RIDGE);
 but18.place(x=219, y=280);
 
+#logica
+
+#criando variavel
+all_values = ""
+
+
+#criando função input, que recebe o evento e adiciona o valor ao all_values e seta o valor no label 
+def input(event):
+    
+    global all_values
+    
+    all_values = all_values + str(event)
+    
+    text_val.set(all_values)
+    
+
+
+     
 window.mainloop()
